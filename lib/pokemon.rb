d7cc9c843c09f68b4d7182df663b62e4ba13dda4
Moomal <moomalmsoomro@gmail.com>
2021-04-23 09:37:29 -0400

    commit tree 77730a1e2ad32afd56511d16423afb5dc8566ffd

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,35 @@
 class Pokemon
+
+    attr_accessor :name, :type, :db
+    attr_reader :id
+
+    def initialize (id=nil, name=nil, type=nil, db)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save (name, type, db)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?,?)
+        SQL
+
+        db.execute(sql, name, type)
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+
+    def self.find (id, db)
+        sql = <<-SQL
+            SELECT *
+            FROM pokemon
+            WHERE id=?
+            LIMIT 1
+        SQL
+
+        db.execute(sql, id).map do |row|
+            Pokemon.new(row[0], row[1], row[2], row[3])
+        end.first
+    end
 end
